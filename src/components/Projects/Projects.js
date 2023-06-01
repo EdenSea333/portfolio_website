f4b3c5d8cdc5a3c6f2c04e6281791a59371c156f
IlliaShevchenko028 <illiashevchenko028@gmail.com>
2023-06-02 01:35:23 +0300

    change

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -18,6 +18,7 @@ const Projects = () => (
               <Hr />
             </TitleContent>
             {/*  <CardInfo className="card-info">{p.description}</CardInfo> */}
+            <h1>test</h1>
             <div>
               <TitleContent>Stack</TitleContent>
               <TagList>
